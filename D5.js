12e9adbe0b3b2929806ee61b90d08a5bf1e70ae1
Weber <vito588p@gmail.com>
2023-09-26 13:50:28 +0800

    docs: updated example in D5

--- D5.js
+++ D5.js
@@ -1,4 +1,17 @@
-//Array Reduce Transformation
+// Array Reduce Transformation
+
+// Example 1:
+// Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+// Output: [2,3,4]
+
+// Example 2:
+// Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+// Output: [1,3,5]
+
+// Example 3:
+// Input: arr = [10,20,30], fn = function constant() { return 42; }
+// Output: [42,42,42]
+
 var reduce = function(nums, fn, init) {
   let count = init;
     

26b09792331544bffc8c6de199f7f442eedccade
Weber <vito588p@gmail.com>
2023-10-06 22:56:18 +0800

    docs: add Day11

---
+++ D11.js
@@ -0,0 +1,29 @@
+// Memoize
+
+// Example 1:
+// Input:
+// fnName = "sum"
+// actions = ["call","call","getCallCount","call","getCallCount"]
+// values = [[2,2],[2,2],[],[1,2],[]]
+// Output: [4,4,1,3,2]
+
+// Example 2:
+// Input:
+// fnName = "factorial"
+// actions = ["call","call","call","getCallCount","call","getCallCount"]
+// values = [[2],[3],[2],[],[3],[]]
+// Output: [2,6,2,2,6,2]
+
+// Example 3:
+// Input:
+// fnName = "fib"
+// actions = ["call","getCallCount"]
+// values = [[5],[]]
+// Output: [8,1]
+
+function memoize(fn) {
+    
+  return function(...args) {
+      
+  }
+}

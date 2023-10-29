fc1a1e877cecf2ac0720afd19c49cf00996a8c2a
Weber <vito588p@gmail.com>
2023-10-29 22:02:25 +0800

    docs: add D18

---
+++ D18.js
@@ -0,0 +1,36 @@
+// Debounce
+
+// Example 1:
+// Input:
+// t = 50
+// calls = [
+// {"t": 50, inputs: [1]},
+// {"t": 75, inputs: [2]}
+// ]
+// Output: [{"t": 125, inputs: [2]}]
+
+// Example 2:
+// Input:
+// t = 20
+// calls = [
+// {"t": 50, inputs: [1]},
+// {"t": 100, inputs: [2]}
+// ]
+// Output: [{"t": 70, inputs: [1]}, {"t": 120, inputs: [2]}]
+
+// Example 3:
+// Input:
+// t = 150
+// calls = [
+// {"t": 50, inputs: [1, 2]},
+// {"t": 300, inputs: [3, 4]},
+// {"t": 300, inputs: [5, 6]}
+// ]
+// Output: [{"t": 200, inputs: [1,2]}, {"t": 450, inputs: [5, 6]}]
+
+var debounce = function(fn, t) {
+    
+  return function(...args) {
+      
+  }
+};
